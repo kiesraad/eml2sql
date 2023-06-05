a7f0aec2bf9f84ba090659782e55e6d5a5c625fc
bert hubert <bert@hubertnet.nl>
2023-06-05 12:54:54 +0200

    add dependencies

--- makewin.sh
+++ makewin.sh
@@ -4,4 +4,4 @@ i686-w64-mingw32-gcc -Iext/sqlite-amalgamation-3420000/ ext/sqlite-amalgamation-
 i686-w64-mingw32-g++ -std=gnu++17 -DMINGW -Iext/sqlite-amalgamation-3420000/ -I ext/pugixml-1.13/src/ emlconv.cc  ext/pugixml-1.13/src/pugixml.cpp sqlwriter.cc  -c  -pthread 
 i686-w64-mingw32-g++ -std=gnu++17 emlconv.o pugixml.o sqlite3.o sqlwriter.o  -static-libgcc -static-libstdc++ -o emlconv -pthread
 i686-w64-mingw32-strip emlconv.exe 
-zip emlconv-$(date +%Y%m%d-%H%M).zip emlconv.exe doPS1 doPS2 useful-views useful-queries tk-and-ps2script ps1script 
+zip emlconv-$(date +%Y%m%d-%H%M).zip emlconv.exe doPS1 doPS2 useful-views useful-queries tk-and-ps2script ps1script makeheader
